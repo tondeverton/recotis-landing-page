76138f1f7e8df9d6bcad52c8fa8e19e8533fa061
Everton Pereira <se@ton.dev.br>
2025-10-19 21:21:00 -0300

    Add base configurations and file movements to use Astro framework

--- js/security.js
+++
@@ -1,8 +0,0 @@
-/*
-This file is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
-You may obtain a copy of the license at https://creativecommons.org/licenses/by-nc/4.0/legalcode.txt
-*/
-
-async function generateToken() {
-    return ''
-}
